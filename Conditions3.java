21aaa445c93f82b21ba0cd68bcacf21905ec5084
PadmshriYadav <yadavpadmshri@gmail.com>
2025-09-17 01:41:02 +0530

    for do while

---
+++ Conditions3.java
@@ -0,0 +1,21 @@
+public class Conditions3 {
+ public static void main (String[] args){
+
+   for (int i = 1; i <= 5; i++) {
+    System.out.print(i);
+}
+
+int i =5;
+while(i>0){
+    System.out.print(i);
+    --i;
+}
+ int j =5;
+ do {
+    System.out.print(j);
+    --j;
+}while(j>0);
+
+ }
+
+ }
