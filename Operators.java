28c610026c70bcd7ff4c4543af6378e129a3f603
PadmshriYadav <yadavpadmshri@gmail.com>
2025-09-16 19:09:48 +0530

    operators

--- Operators.java
+++ Operators.java
@@ -6,15 +6,25 @@
 // ---> Bitwise(&,!)
 
 //Precedence-Associativity
-//()--> (*)--(/)--(%)-->(+)--(-)
+//()-->++,---> (*)--(/),(%)-->(+),(-)
 
 
-//datatype of expressions
+//datatype of expressions int i=5
+//use then increment Post i++--->5,6
+//Pre ++i--->6,6
 
 public class operators{
     public static void main (String[] args){
 
+       float a = 7/4*9/2;
+       System.out.println(a); //8.0
+
+         float b = 7/4f*9/2;
+       System.out.println(b); //7.875
        
+       int b =65;
+       System.out.println((char)(b));//A
+
 
     }
 }
