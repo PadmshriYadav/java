9b359f75c78ea2715ea54a19d9c75ac471de3081
PadmshriYadav <yadavpadmshri@gmail.com>
2025-09-16 18:35:38 +0530

    tractor

---
+++ Operators.java
@@ -0,0 +1,20 @@
+// //operators
+// --->arthematic(+_*/%) % gives remain
+// --->relational (==,!=,>,<,>=,<=) return a boolean 
+// --->Logical (&&,||,!) return a boolean
+// --->Assignment(=,+=,-=,*=,/=,%=) assign values
+// ---> Bitwise(&,!)
+
+//Precedence-Associativity
+//()--> (*)--(/)--(%)-->(+)--(-)
+
+
+//datatype of expressions
+
+public class operators{
+    public static void main (String[] args){
+
+       
+
+    }
+}
